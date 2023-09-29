2ea7db33600c644ef67e22e8405969d8fe473a5d
PATRIOT <113564993+MATBEN2112@users.noreply.github.com>
2023-09-29 15:27:50 +0300

    Update CustomSoundLoader.py

--- CustomSoundLoader.py
+++ CustomSoundLoader.py
@@ -1,12 +1,21 @@
 from kivy.core.audio import SoundLoader
 
-sound = SoundLoader.load('1.mp3')
-if sound:
-    print("Sound found at %s" % sound.source)
-    print("Sound is %.3f seconds" % sound.length)
-    sound.play()
-sound = SoundLoader.load('file_example_MP3_1MG.mp3')
-if sound:
-    print("Sound found at %s" % sound.source)
-    print("Sound is %.3f seconds" % sound.length)
-    sound.play()
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class TestApp(App):
+
+    def build(self):
+        # return a Button() as a root widget
+        return Button(text='hello world', on_release=self.play_a)
+    def play_a(self, o):
+        sound = SoundLoader.load('file_example_MP3_1MG.mp3')
+        sound.play()
+        
+
+
+if __name__ == '__main__':
+    TestApp().run()
