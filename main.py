cd4b14e3f4c014eda0876b40e33fa5e376893ef4
PATRIOT <113564993+MATBEN2112@users.noreply.github.com>
2023-09-30 03:52:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from kivy.core.audio import SoundLoader
-
 import kivy
-kivy.require('1.0.7')
+from kivymd.uix.boxlayout import MDBoxLayout
+#kivy.require('1.0.7')
 
 from kivy.app import App
 from kivy.uix.button import Button
 from ffpyplayer.player import MediaPlayer
 
+from kivy.uix.videoplayer import VideoPlayer
 
 class TestApp(App):
 
     def build(self):
         # return a Button() as a root widget
-        return Button(text='hello world', on_release=self.play_a)
-    def play_a(self, o):
-        self.sound = SoundLoader().load('seg.ts')
-        self.sound.play()
-        #print(self.sound.extensions)
-        print(dir(self.sound))
-        
+        w = MDBoxLayout()
+        w.add_widget(Button(text='mp3', on_release=self.play_mp3))
+        w.add_widget(Button(text='ts', on_release=self.play_ts))
+        w.add_widget(Button(text='m4a', on_release=self.play_m4a))
+        return w
+    def play_mp3(self, o):
+
+        self.sound = MediaPlayer('1.mp3')
+
+    def play_ts(self, o):
+        self.sound = MediaPlayer('seg.ts')
+
+    def play_m4a(self, o):
+
+        self.sound = MediaPlayer('1.m4a')
 
 
 if __name__ == '__main__':
