5240dd7bce2ae521d3b3e17dd8c3664d33c83408
PATRIOT <113564993+MATBEN2112@users.noreply.github.com>
2023-10-07 16:56:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -592,17 +592,17 @@ class LoginApp(MDApp):
             self.sound = IOSPlayer(self.track[1],author = self.track[2], song=self.track[3])
             self.sound.play()
             
-            [rsetattr(i, 'ids.song_progress.max', self.sound.length) for i in self.audio_bar]
+            [rsetattr(i, 'ids.song_progress.max', self.sound.get_length()) for i in self.audio_bar]
             [rsetattr(i, 'ids.song_progress.value', 0) for i in self.audio_bar]
 
             # needed conversion from obj  c double to float
             print(f'Audio bar max value at start: {self.player_screen.ids.song_progress.max}')
-            print(f'The value: {self.sound.length}')
-            self.player_screen.ids.song_progress.max = self.sound.length
+            print(f'The value: {self.sound.get_length()}')
+            self.player_screen.ids.song_progress.max = self.sound.get_length()
             self.player_screen.ids.song_progress.value = 0
             print(f'Audio bar max value after init value: {self.player_screen.ids.song_progress.max}')
             #
-            self.player_screen.ids.song_len.text = time.strftime('%M:%S', time.gmtime(self.sound.length))
+            self.player_screen.ids.song_len.text = time.strftime('%M:%S', time.gmtime(self.sound.get_length()))
 
         elif 'sound' in dir(self) and args: # Seek audio row
             if True: #slider.collide_point(touch.x, touch.y):
