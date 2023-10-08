63471297340b2387a6fa6f41ba2effd4abc14327
PATRIOT <113564993+MATBEN2112@users.noreply.github.com>
2023-10-08 17:51:00 +0300

    Update CustomSoundLoader.py

--- CustomSoundLoader.py
+++ CustomSoundLoader.py
@@ -14,10 +14,11 @@ class IOSPlayer(Sound):
         IOS_player = autoclass('IOS_player')
         track_arr = NSMutableArray.arrayWithCapacity_(len(track_list))
         for track in track_list:
+            img = NSString.alloc().initWithUTF8String_(track[4])
             author = NSString.alloc().initWithUTF8String_(track[2])
             song = NSString.alloc().initWithUTF8String_(track[3])
             fn = NSString.alloc().initWithUTF8String_(track[1])
-            track_arr.addObject_(objc_arr(track[0], fn, author, song))
+            track_arr.addObject_(objc_arr(track[0], fn, author, song, img))
             
         self.player = IOS_player.alloc().initWithtrackList_key_(track_arr,key)
 
