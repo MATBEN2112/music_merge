3966b78e5d7102aea6f7c8799b77777e801145f6
PATRIOT <113564993+MATBEN2112@users.noreply.github.com>
2023-10-09 00:57:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -582,7 +582,7 @@ class LoginApp(MDApp):
             self.track = track
             self.album_key = self.album_screen.key if self.manager.current=='album' else None
             self.track_list = self.meta.track_list(key = self.album_key) ###
-            player.load_playlist(self.track_list, self.album_key)
+            player.load_playlist(self.track_list, self.track[0])
             player.play()
            
         else: # Unpause already loaded audio
